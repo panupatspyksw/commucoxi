15a9480e0ea8399941ff3175d9cd7c72d59bcf36
panupatspyksw <pnpspyksw@gmail.com>
2022-05-09 21:31:00 +0700

    first commit

--- src/components/TransitionPage.js
+++ src/components/TransitionPage.js
@@ -1,7 +1,5 @@
 import { motion } from 'framer-motion';
-import gsap from 'gsap';
-import ScrollTrigger from 'gsap/ScrollTrigger';
-import { React, Fragment } from 'react';
+import { React } from 'react';
 const animationConfiguration = {
   initial: { opacity: 0, zIndex: -1, position: 'relative' },
   animate: { opacity: 1, zIndex: -1 },
@@ -20,10 +18,6 @@ const Transitions = ({ children }) => {
       <div>{children}</div>
     </motion.div>
   );
-};
-
-const gsapscrollanimation = () => {
-  var tl = gsap.timeline();
 };
 
 export default Transitions;
